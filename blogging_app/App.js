f7ebee6194b81fe4af4ab4196c542f0b89a86432
Aarushi Vohra <aarushivohra01@gmail.com>
2021-09-18 16:49:59 +0530

    added signup

--- blogging_app/App.js
+++ blogging_app/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Home from './views/Home';
 import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
 
 const app = () => {
 
   return(
     // <Home/>
-    <SignIn/>
+    // <SignIn/>
+    <SignUp />
   )
   
 };
