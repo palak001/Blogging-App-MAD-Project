9a6df1e5901aa29997a4cedbe9a1468671ab1096
Aarushi Vohra <aarushivohra01@gmail.com>
2021-11-09 12:49:57 +0530

    text editor

--- blogging_app/views/TextEditor.js
+++ blogging_app/views/TextEditor.js
@@ -1,34 +1,139 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
-import textEditorStyles from '../styles/textEditorStyles';
-import {lgrey, marigold} from '../styles/theme';
+import React, { useRef, useState } from "react";
+import { StyleSheet, Text, ScrollView, View } from "react-native";
+import {
+  actions,
+  defaultActions,
+  RichEditor,
+  RichToolbar,
+} from "react-native-pell-rich-editor";
+import HTMLView from "react-native-htmlview";
 
-const textEditor = () => {
-  const [blog, setBlog] = useState('');
+const editorScreen = () => {
+  const strikethrough = require("../assets/icons/strikethrough-solid.svg");
+  const video = require("../assets/icons/video-solid.svg"); 
+
+  const RichText = useRef(); 
+
+  const [article, setArticle] = useState("");
+
+  function editorInitializedCallback() {
+    RichText.current?.registerToolbar(function (items) {
+      // items contain all the actions that are currently active
+      console.log(
+        "Toolbar click, selected items (insert end callback):",
+        items
+      );
+    });
+  }
+
+  // Callback after height change
+  function handleHeightChange(height) {
+    // console.log("editor height change:", height);
+  }
+
+  function onPressAddImage() {
+    // you can easily add images from your gallery
+    RichText.current?.insertImage(
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/100px-React-icon.svg.png"
+    );
+  }
+
+
+  function insertVideo() {
+    // you can easily add videos from your gallery
+    RichText.current?.insertVideo(
+      "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4"
+    );
+  }
+
+
 
   return (
-    <View style={textEditorStyles.outerView}>
-      <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
-        <TouchableOpacity>
-          <Text style={textEditorStyles.textStyle} >X</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log(blog)}>
-          <Text style={textEditorStyles.textStyle}>Publish</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TextInput
-          style={textEditorStyles.editor}
-          selectionColor={marigold}
-          autoFocus={true}
-          autoCapitalize="sentences"
-          multiline={true}
-          onChangeText={setBlog}
-          value={blog}
-        />
-      </View>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.text}>Editor</Text>
+      <RichEditor
+        disabled={false}
+        containerStyle={styles.editor}
+        ref={RichText}
+        style={styles.rich}
+        placeholder={"Start Writing Here"}
+        onChange={(text) => setArticle(text)}
+        editorInitializedCallback={editorInitializedCallback}
+        onHeightChange={handleHeightChange}
+      />
+      <RichToolbar
+        style={[styles.richBar]}
+        editor={RichText}
+        disabled={false}
+        iconTint={"purple"}
+        selectedIconTint={"pink"}
+        disabledIconTint={"purple"}
+        onPressAddImage={onPressAddImage}
+        iconSize={40}
+        actions={[
+          "insertVideo",
+          ...defaultActions,
+          actions.setStrikethrough,
+          actions.heading1,
+        ]}
+        // map icons for self made actions
+        iconMap={{
+          [actions.heading1]: ({ tintColor }) => (
+            <Text style={[styles.tib, { color: tintColor }]}>H1</Text>
+          ),
+          [actions.setStrikethrough]: strikethrough,
+          ["insertVideo"]: video,
+        }}
+        insertVideo={insertVideo}
+      />
+      <Text style={styles.text}>Result</Text>
+      <HTMLView value={article} stylesheet={styles} />
+    </ScrollView>
   );
 };
 
-export default textEditor;
+
+export default editorScreen;
+
+
+const styles = StyleSheet.create({
+  /********************************/
+  /* styles for html tags */
+  a: {
+    fontWeight: "bold",
+    color: "purple",
+  },
+  div: {
+    fontFamily: "monospace",
+  },
+  p: {
+    fontSize: 30,
+  },
+  /*******************************/
+  container: {
+    flex: 1,
+    marginTop: 40,
+    backgroundColor: "#F5FCFF",
+  },
+  editor: {
+    backgroundColor: "black",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  rich: {
+    minHeight: 300,
+    flex: 1,
+  },
+  richBar: {
+    height: 50,
+    backgroundColor: "#F5FCFF",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  tib: {
+    textAlign: "center",
+    color: "#515156",
+  },
+});
---
+++ blogging_app/views/TextEditor1.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import textEditorStyles from '../styles/textEditorStyles';
+import {lgrey, marigold} from '../styles/theme';
+
+const textEditor = () => {
+  const [blog, setBlog] = useState('');
+
+  return (
+    <View style={textEditorStyles.outerView}>
+      <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+        <TouchableOpacity>
+          <Text style={textEditorStyles.textStyle} >X</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log(blog)}>
+          <Text style={textEditorStyles.textStyle}>Publish</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          style={textEditorStyles.editor}
+          selectionColor={marigold}
+          autoFocus={true}
+          autoCapitalize="sentences"
+          multiline={true}
+          onChangeText={setBlog}
+          value={blog}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default textEditor;
